fe2fcdaee2689caeadbdf82a5706489576ecfe53
r57zone <r57zone@gmail.com>
2017-07-02 14:30:30 +0400

    update

--- OpenVR/samples/driver_sample/driver_sample.cpp
+++ OpenVR/samples/driver_sample/driver_sample.cpp
@@ -272,7 +272,7 @@ public:
 		vr::VRProperties()->SetBoolProperty( m_ulPropertyContainer, Prop_IsOnDesktop_Bool, false );
 
 		//Debug mode activate Windowed Mode (borderless fullscreen) on "Headset Window" and you can move window to second screen with buttons (Shift + Win + Right or Left) 
-		vr::VRProperties()->SetBoolProperty(m_ulPropertyContainer, Prop_DisplayDebugMode_Bool, true);
+		//vr::VRProperties()->SetBoolProperty(m_ulPropertyContainer, Prop_DisplayDebugMode_Bool, true);
 
 		// Icons can be configured in code or automatically configured by an external file "drivername\resources\driver.vrresources".
 		// Icon properties NOT configured in code (post Activate) are then auto-configured by the optional presence of a driver's "drivername\resources\driver.vrresources".
